5f9edccd51d0265c81078108b7aeba82813dca01
AYoux <ayman.youssef@gmail.com>
2014-01-10 17:29:50 +0200

    Added the ability to show accounbt types and drill in them -- but with slight error that its always Assets

--- app/controllers/account_types_controller.rb
+++ app/controllers/account_types_controller.rb
@@ -1,8 +1,11 @@
 class AccountTypesController < ApplicationController
   def index
+  	@accounttype = AccountType.all
   end
 
   def show
+  	@accounttype = AccountType.find_by(params[:name])
+  	@accounts = Account.all
   end
 
   def create
